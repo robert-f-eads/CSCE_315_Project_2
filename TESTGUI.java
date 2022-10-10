3c512e2db275717220fe2c2181061ad631b108d9
alexiahassan <alexiahassan@tamu.edu>
2022-10-10 15:17:21 -0500

    Added search bar to GUI

--- TESTGUI.java
+++ TESTGUI.java
@@ -1,72 +1,96 @@
 import javax.swing.*;
+import javax.swing.border.*;
+
+
 import java.awt.*;
+import java.awt.event.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import javax.imageio.ImageIO;
-import javax.swing.ImageIcon;
 
 class TESTGUI{
     public static void main(String args[]){
-       JFrame frame = new JFrame("My First GUI");
-       frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-       Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-       frame.setSize(screenSize.width - 100, screenSize.height);
-       //JButton button = new JButton("Press");
-       //frame.getContentPane().add(button); // Adds Button to content pane of frame
+
+		//Create JFrame and initial settings
+		JFrame frame = new JFrame("Smoothie King");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+		frame.setSize(screenSize.width - 100, screenSize.height);
 
 
-       /*JLabel label = new JLabel();
-		//label.setText("Hi");
-		//label.setIcon(icon);
-		label.setVerticalAlignment(JLabel.TOP);
-		label.setHorizontalAlignment(JLabel.LEFT);
-		//label.setBounds(100, 100, 75, 75);*/
-		
-		JPanel redPanel = new JPanel();
-		redPanel.setBackground(Color.red);
-		redPanel.setBounds(1125, 0, 275, 1000);
-		redPanel.setLayout(new BorderLayout());
-		
+		//Logo panel will house Smoothie King logo in top left 
 		JPanel logoPanel = new JPanel();
 		logoPanel.setBackground(Color.white);
 		logoPanel.setBounds(0, 0, 1125, 75);
 		logoPanel.setLayout(new BorderLayout());
 
-        JPanel orangePanel = new JPanel();
-		orangePanel.setBackground(Color.orange);
-		orangePanel.setBounds(0, 75, 1125, 75);
-		orangePanel.setLayout(new BorderLayout());
+		//Create Label as icon and add it to the logo panel
+		JLabel picLabel = new JLabel();
+		picLabel.setIcon(new ImageIcon(new ImageIcon("logo.png").getImage().getScaledInstance(400, 70, Image.SCALE_SMOOTH)));
+		logoPanel.add(picLabel);
+
+		
+		//Search panel will house static search bar in top left
+		JPanel searchPanel = new JPanel();
+		searchPanel.setBackground(Color.orange);
+		searchPanel.setBounds(13, 87, 1100, 50);
+		searchPanel.setLayout(new BorderLayout());
+
+		//Create text component of search bar
+		Font searchFont = new Font("SansSerif", Font.PLAIN, 20); //font used in text box
+		JTextField searchTextField = new JTextField();
+		searchTextField.setPreferredSize(new Dimension(1050,50));
+		searchTextField.setHorizontalAlignment(JTextField.LEFT);
+		searchTextField.setLocation(0, 75);
+		searchTextField.setFont(searchFont);
+
+		//Create button component of search bar
+		JButton searchButton = new JButton(new ImageIcon(((new ImageIcon("searchIcon.png")).getImage()).getScaledInstance(43, 43, java.awt.Image.SCALE_SMOOTH)));
+		//JButton searchButton = new JButton(searchIcon.getScaledInstance( 50, 50,  java.awt.Image.SCALE_SMOOTH));
+		searchButton.setPreferredSize(new Dimension(50,50));
+		searchButton.addActionListener(new ActionListener() 
+			{
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					String typedValue = searchTextField.getText();
+					System.out.println(typedValue);
+				}
+			}
+		);
+
+		//Set searchButton colors
+		searchButton.setForeground(Color.BLACK);
+		searchButton.setBackground(Color.WHITE);
+
+
+		//Set border colors for search bar and search button
+		Border line = new LineBorder(Color.BLACK);
+		searchTextField.setBorder(line);
+		searchButton.setBorder(line);
+
+		//Add both search bar and search button to panel in appropriate place 
+		searchPanel.add(searchButton, BorderLayout.LINE_START);
+		searchPanel.add(searchTextField, BorderLayout.LINE_END);
+
+		JPanel redPanel = new JPanel();
+		redPanel.setBackground(Color.red);
+		redPanel.setBounds(1125, 0, 275, 1000);
+		redPanel.setLayout(new BorderLayout());
+		
+
+
 		
 		JPanel greenPanel = new JPanel();
 		greenPanel.setBackground(Color.green);
 		greenPanel.setBounds(0, 0, 20, 10);
 		greenPanel.setLayout(new BorderLayout());
 
-         BufferedImage myPicture;
-        JLabel picLabel = new JLabel();
-       /*  try {
-            myPicture = ImageIO.read(new File("logo.png"));
-            picLabel = new JLabel(new ImageIcon(myPicture));
-            logoPanel.add(picLabel);    
-        } catch (IOException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-        picLabel.setPreferredSize(new Dimension(100, 50));
-        picLabel.setMaximumSize(new Dimension(100, 50));*/
 
-        picLabel.setIcon(new ImageIcon(new ImageIcon("logo.png").getImage().getScaledInstance(400, 70, Image.SCALE_SMOOTH)));
                 
-		/*JFrame frame = new JFrame();
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setLayout(null);
-		frame.setSize(750,750);
-		frame.setVisible(true);	*/
-		logoPanel.add(picLabel);
 		frame.add(redPanel);
 		frame.add(logoPanel);
-        frame.add(orangePanel);
+        frame.add(searchPanel);
 		frame.add(greenPanel);
 
         frame.setVisible(true);
