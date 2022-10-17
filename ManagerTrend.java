4edea97b8e5e8db695a2a6af3ea7e693b809f145
alexiahassan <alexiahassan@tamu.edu>
2022-10-17 12:06:19 -0500

    Fixed multiple charts in trends page, made button selections in trend page

--- ManagerTrend.java
+++ ManagerTrend.java
@@ -3,6 +3,9 @@ import java.awt.*;
 
 public class ManagerTrend extends ManagerViewScreen {
     private orderViewFunctions ovf;
+    JButton currentlySelectedButton;
+    static Color blueHighlight = new Color(184, 204, 220);
+
 
     public ManagerTrend(ManagerView managerView) {
         super(managerView);
@@ -14,13 +17,54 @@ public class ManagerTrend extends ManagerViewScreen {
         int buttonHeight = 150;
         JButton back = managerView.createButton("Back", buttonWidth, buttonHeight);
         JButton week = managerView.createButton("1 Week", buttonWidth, buttonHeight);
+        week.addActionListener(e -> {
+            if (currentlySelectedButton != null) {
+                currentlySelectedButton.setBackground(Color.white);
+            }
+            currentlySelectedButton = week;
+            week.setBackground(blueHighlight);
+        });
+
         JButton week2 = managerView.createButton("2 Weeks", buttonWidth, buttonHeight);
+        week2.addActionListener(e -> {
+            if (currentlySelectedButton != null) {
+                currentlySelectedButton.setBackground(Color.white);
+            }
+            currentlySelectedButton = week2;
+            week2.setBackground(blueHighlight);
+        });
+
         JButton month = managerView.createButton("1 Month", buttonWidth, buttonHeight);
+        month.addActionListener(e -> {
+            if (currentlySelectedButton != null) {
+                currentlySelectedButton.setBackground(Color.white);
+            }
+            currentlySelectedButton = month;
+            month.setBackground(blueHighlight);
+        });
+
         JButton quarter = managerView.createButton("1 Quarter", buttonWidth, buttonHeight);
+        quarter.addActionListener(e -> {
+            if (currentlySelectedButton != null) {
+                currentlySelectedButton.setBackground(Color.white);
+            }
+            currentlySelectedButton = quarter;
+            quarter.setBackground(blueHighlight);
+        });
+
         JButton year = managerView.createButton("1 Year", buttonWidth, buttonHeight);
+        year.addActionListener(e -> {
+            if (currentlySelectedButton != null) {
+                currentlySelectedButton.setBackground(Color.white);
+            }
+            currentlySelectedButton = year;
+            year.setBackground(blueHighlight);
+        });
+
         JButton generate = managerView.createButton("Generate", buttonWidth, buttonHeight);
 
         JLabel inventory = managerView.createLabel("Trends", buttonWidth, buttonHeight);
+        
         inventory.setFont(new Font("SansSerif", Font.PLAIN, 28));
         inventory.setBackground(Color.green);
         inventory.setAlignmentX(Component.CENTER_ALIGNMENT);
@@ -35,16 +79,6 @@ public class ManagerTrend extends ManagerViewScreen {
             managerView.myFrame.dispose();
         });
 
-        // TODO make all other trends also be allowed to generate, expand gui to include selection of these trends
-        // TODO make sure when generate is pressed multiple times, it only adds one table total
-        generate.addActionListener(e -> {
-            JTable sales = new JTable(managerView.resultSetToTableModel(null, 
-                ovf.generateSalesReportBetweenDates(new dateStruct("2020-10-10"), 
-                    new dateStruct("2022-10-10"), true)));
-            managerView.borderPanel.add(new JScrollPane(sales));
-            managerView.myFrame.revalidate();
-            managerView.myFrame.repaint();
-        });
 
     
 
@@ -92,6 +126,20 @@ public class ManagerTrend extends ManagerViewScreen {
         managerView.borderPanel.revalidate();
         managerView.borderPanel.repaint();
         managerView.myFrame.add(managerView.borderPanel);
+
+
+        
+        // TODO make all other trends also be allowed to generate, expand gui to include selection of these trends
+        generate.addActionListener(e -> {
+            JTable sales = new JTable(managerView.resultSetToTableModel(null, 
+                ovf.generateSalesReportBetweenDates(new dateStruct("2020-10-10"), 
+                    new dateStruct("2022-10-10"), true)));
+            managerView.borderPanel.removeAll();
+            managerView.borderPanel.add(pageStart);
+            managerView.borderPanel.add(new JScrollPane(sales));
+            managerView.myFrame.revalidate();
+            managerView.myFrame.repaint();
+        });
     }
 
     JTextField formatTextArea(JTextField b) {
