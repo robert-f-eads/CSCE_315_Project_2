d8699883842c8ea91241a5cebc6b94b7ab2498a9
shreyesk <sshreyes.kaliyur@gmail.com>
2022-10-18 20:47:35 -0500

    javadocs manager view PP Emma, Robert

--- HintTextField.java
+++ HintTextField.java
@@ -4,9 +4,18 @@ import java.awt.event.FocusListener;
 import java.awt.*;
 
 
+/**
+ * @author Robert Eads
+ */
 public class HintTextField extends JTextField implements FocusListener {
     private String myHint;
 
+    /**
+     * Parametrized constructor for a TextField that acts like it has a hint 
+     * @param hint the hint to be displayed
+     * @param width the width of the text field
+     * @param height the height of the text field
+     */
     public HintTextField(String hint, int width, int height) {
         super(hint);
         myHint = hint;
@@ -15,6 +24,10 @@ public class HintTextField extends JTextField implements FocusListener {
         this.setForeground(Color.lightGray);
     }
 
+    /**
+     * Clears the hint to prepare the text field for input
+     * @param e the event which causes the text field to gain focus
+     */
     @Override
     public void focusGained(FocusEvent e) {
         if(this.getText().equals(myHint)) {
@@ -23,6 +36,10 @@ public class HintTextField extends JTextField implements FocusListener {
         }
     }
 
+    /**
+     * Resets the text field to display the hint, grayed out
+     * @param e the event which causes the text field to lose focus
+     */
     @Override
     public void focusLost(FocusEvent e) {
         if(this.getText().equals("")) {
